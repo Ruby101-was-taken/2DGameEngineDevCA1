f599f753f1ba44483e51a93d312076301372b688
Ruby101-was-taken <abusinessemailkinda@gmail.com>
2023-10-10 12:25:00 +0100

    Jumping Fixes
    You can now jump properly. Still a lot of jank but I blame Unity not my perfect code

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -5,41 +5,64 @@ using UnityEngine;
 public class CharacterController : MonoBehaviour
 {
     public float jumpForce = 10.0f;
-    public float maxGroundAngle = 45.0f;
     public float movementSpeed = 10f;
 
+    public bool inBall = false;
+    public bool jumped = false;
+
+    public LayerMask whatIsGround;
+    public Transform groundCheckPoint;
+    public float groundCheckRadius = 0.05f;
 
     private bool isGrounded = false;
     private Rigidbody2D body;
+    private Animator anim;
+
+    private int kTime = 0;
+
 
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
 
+        isGrounded = Physics2D.OverlapBox(groundCheckPoint.position, new Vector2(0.1f, 0.2f), 0, whatIsGround);
+        if (isGrounded)
+        {
+            inBall = false;
+            kTime = 20;
+        }
+        else if(kTime > 0)
+        {
+            kTime -= 1;
+        }
         // Handle movement
         body.velocity = new Vector2(horizontalInput * movementSpeed, body.velocity.y);
 
         // Jumping
-        if (isGrounded && Input.GetButtonDown("Jump"))
+        if ((isGrounded || kTime>0) && Input.GetButtonDown("Jump"))
         {
+            inBall = true;
             isGrounded = false;
-            body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            body.velocityY = jumpForce;
+            kTime = 0;
+        }
+        Debug.Log("Ktime: " + kTime);
+        anim.SetBool("InBall", inBall) ;
+
+        if(transform.position.y < -20)
+        {
+            body.velocityX = 0;
+            body.velocityY = 0;
+            transform.position = new Vector3(0, 0, 0);
         }
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == ("Ground"))
-        {
-            isGrounded = true;
-            Debug.Log("Grounded");
-        }
-    }
 
 }
 
